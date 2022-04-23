087bcba6822ca073fed63739813604dd794fc772
hridoy002 <hridoyalhasan2@gmail.com>
2022-04-24 04:03:01 +0600

    about sec

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import hridoy from '../../images/hridoy.JPG'
+import './About.css'
 const About = () => {
     return (
-        <div>
-            <h2>About</h2>
+        <div className='container about'>
+            <h2>About Me</h2>
+            <div class="square">
+                <div>
+                    <img src={hridoy} alt="" />
+                </div>
+
+
+                <p> <b>INTRODUCTION: </b>Hi! I am Mohammad Hridoy Ahmmed.I born in 20 April in Brahmanbaria.Brahmanbaria is also my origin.I live in now Mymensingh.
+                </p>
+            </div>
+
         </div>
     );
 };
