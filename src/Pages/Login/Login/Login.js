ffbe5e87fdf21107cc50274de18924cb36123c84
hridoy002 <hridoyalhasan2@gmail.com>
2022-04-24 16:22:11 +0600

    error handeling

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -3,6 +3,8 @@ import { Button, Form } from 'react-bootstrap';
 import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
     const navigate = useNavigate();
@@ -25,13 +27,17 @@ const Login = () => {
     if (user) {
         navigate('/')
     }
+    let errorMessage;
+    if (error) {
+           errorMessage=<p className='text-danger'>Error: {error.message}</p>;}
+
     // password reset auth hook 
     const [sendPasswordResetEmail] = useSendPasswordResetEmail(
         auth
     )
     const [email, setEmail] = useState();
 
-
+    
 
 
     return (
@@ -40,24 +46,26 @@ const Login = () => {
             <Form onSubmit={handleLoginSubmit} className='w-75 mx-auto'>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
 
-                    <Form.Control onBlur={(e) => setEmail(e.target.value)} type="email" name='email' placeholder="Enter email" required/>
+                    <Form.Control onBlur={(e) => setEmail(e.target.value)} type="email" name='email' placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control type="password" name='password' placeholder="Password" required/>
+                    <Form.Control type="password" name='password' placeholder="Password" required />
                 </Form.Group>
-                <p>Haven't any account?<Link to='/register' className='text-info text-decoration-none'> Please Register.</Link></p>
-                <p>Forget Your Password?<button onClick={async () => {
-                    await sendPasswordResetEmail(email);
-                    if(email){
-                        alert('Sent email');
-                    }
-                }} className='btn btn-link text-decoration-none text-danger'>Reset Password</button></p>
-
+                
+                {errorMessage}
                 <Button className='btn-info text-light w-50' variant="primary" type="submit">
                     Login
                 </Button>
+                <p>Haven't any account?<Link to='/register' className='text-info text-decoration-none'> Please Register.</Link></p>
+                <p>Forget Your Password?<button onClick={async () => {
+                    await sendPasswordResetEmail(email);
+                    if (email) {
+                        toast('Sent email');
+                    }
+                }} className='btn btn-link text-decoration-none text-info'>Reset Password</button></p>
             </Form>
+            <ToastContainer/>
 
         </div>
     );
